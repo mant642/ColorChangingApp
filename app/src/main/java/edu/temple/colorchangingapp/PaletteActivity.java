ee98254aa3ba50d904ad4cf6cc066393e3cb16f8
Mat <tuf62412@temple.edu>
2020-10-06 17:35:11 -0400

    Added additional class ColorAdapter which extends BaseAdapter, to populate GridView. Implemented basic toast functionality to verify validity.

--- app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
@@ -2,18 +2,54 @@ package edu.temple.colorchangingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity {
+    GridView gridView;
+    ArrayList<String> colorList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        colorList = new ArrayList<>();
+        colorList.add("white");
+        colorList.add("blue");
+        colorList.add("cyan");
+        colorList.add("darkgrey");
+        colorList.add("grey");
+        colorList.add("green");
+        colorList.add("lightgrey");
+        colorList.add("magenta");
+        colorList.add("red");
+
+        gridView = findViewById(R.id.color_grid);
+
+        ColorAdapter colorAdapter = new ColorAdapter(this, colorList);
+        gridView.setAdapter(colorAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                String selection = parent.getItemAtPosition(position).toString();
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(context, selection, duration).show();
+            }
+        });
+
+        /*
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,11 +57,16 @@ public class PaletteActivity extends AppCompatActivity {
                 showSecondActivity();
             }
         });
+         */
+
+
     }
 
     // Launching an activity with an intent wasn't working previously because it needed to happen in a method, I guess?
+    /*
     public void showSecondActivity () {
         Intent intent = new Intent(this, CanvasActivity.class);
         startActivity(intent);
     }
+     */
 }
