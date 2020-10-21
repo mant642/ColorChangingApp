73adce375d01615b3f5dd7e294cb3a2305a2e3de
Mat <tuf62412@temple.edu>
2020-10-20 23:58:37 -0400

    Finished assignment specifications by changing color upon selection, alongside existing text change. Further commits are necessary to improve appearance and remove unneeded comments, but otherwise complete.

--- app/src/main/java/edu/temple/colorchangingapp/CanvasFragment.java
+++ app/src/main/java/edu/temple/colorchangingapp/CanvasFragment.java
@@ -1,5 +1,7 @@
 package edu.temple.colorchangingapp;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class CanvasFragment extends Fragment {
     TextView textView;
+    FrameLayout frameLayout;
+    String[] colorCompare;
 
     public CanvasFragment() {
         // Required empty public constructor
@@ -27,10 +32,42 @@ public class CanvasFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_canvas, container, false);
         textView = v.findViewById(R.id.display_name);
+        frameLayout = v.findViewById(R.id.frame_layout2);
         return v;
     }
 
     public void updateTextView (String selection) {
         textView.setText(selection);
+
+        Resources res = getResources();
+        colorCompare = res.getStringArray(R.array.color_list);
+
+        if (selection.equals(colorCompare[0])) {
+            frameLayout.setBackgroundColor(Color.WHITE);
+        }
+        if (selection.equals(colorCompare[1])) {
+            frameLayout.setBackgroundColor(Color.BLUE);
+        }
+        if (selection.equals(colorCompare[2])) {
+            frameLayout.setBackgroundColor(Color.CYAN);
+        }
+        if (selection.equals(colorCompare[3])) {
+            frameLayout.setBackgroundColor(Color.DKGRAY);
+        }
+        if (selection.equals(colorCompare[4])) {
+            frameLayout.setBackgroundColor(Color.GRAY);
+        }
+        if (selection.equals(colorCompare[5])) {
+            frameLayout.setBackgroundColor(Color.GREEN);
+        }
+        if (selection.equals(colorCompare[6])) {
+            frameLayout.setBackgroundColor(Color.LTGRAY);
+        }
+        if (selection.equals(colorCompare[7])) {
+            frameLayout.setBackgroundColor(Color.MAGENTA);
+        }
+        if (selection.equals(colorCompare[8])) {
+            frameLayout.setBackgroundColor(Color.RED);
+        }
     }
 }
